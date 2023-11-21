163bf3ab43066a0a2959f728a05a52aa1519d70b
Guschin Mikhail <mikhail.guschin@arkcom.ru>
2023-11-21 14:00:48 +0300

    [wallets,react] Pelagus wallet connector override RPC methods

--- packages/wallets/src/evm/connectors/pelagus/getInjectedPelagusProvider.ts
+++ packages/wallets/src/evm/connectors/pelagus/getInjectedPelagusProvider.ts
@@ -1,16 +1,49 @@
-import { Ethereum } from "../injected/types";
-import { assertWindowEthereum } from "../../utils/assertWindowEthereum";
+import {Ethereum} from "../injected/types";
+import {assertWindowEthereum} from "../../utils/assertWindowEthereum";
 
-export function getInjectedPelagusProvider(): Ethereum | undefined {
+interface QEthereum extends Ethereum {
+  request(args: any): Promise<any>;
+}
+
+export function getInjectedPelagusProvider(): QEthereum | undefined {
   if (typeof window === "undefined") {
     return;
   }
 
-  function getReady(ethereum?: Ethereum): Ethereum | undefined {
+  function getReady(ethereum?: QEthereum): QEthereum | undefined {
     const isPelagus = !!ethereum?.isPelagus;
 
     if (!isPelagus) {
       return;
+    }
+
+    // override some QUAI methods
+    if ("request" in ethereum) {
+      const origRequest = ethereum.request;
+      ethereum.request = async function (args: any) {
+        switch (args.method) {
+          case "eth_accounts":
+            args.method = "quai_accounts";
+            break;
+          case "eth_chainId":
+            args.method = "quai_chainId";
+            break;
+          case "eth_requestAccounts":
+            args.method = "quai_requestAccounts";
+            break;
+          case "wallet_requestPermissions":
+            if (Array.isArray(args.params)) {
+              args.params.forEach((par: any) => {
+                if (par.eth_accounts) {
+                  par.quai_accounts = par.eth_accounts
+                  delete par.eth_accounts
+                }
+              })
+            }
+            break;
+        }
+        return origRequest(args);
+      }
     }
 
     return ethereum;
--- packages/wallets/src/evm/connectors/pelagus/index.ts
+++ packages/wallets/src/evm/connectors/pelagus/index.ts
@@ -1,37 +1,25 @@
 import {AsyncStorage} from "../../../core/AsyncStorage";
 import {
-  AddChainError,
-  ChainNotConfiguredError,
-  ConnectorNotFoundError, ProviderRpcError,
+  ConnectorNotFoundError,
   ResourceUnavailableError,
-  RpcError, SwitchChainError,
+  RpcError,
   UserRejectedRequestError,
 } from "../../../lib/wagmi-core/errors";
 import {walletIds} from "../../constants/walletIds";
 import {InjectedConnector, InjectedConnectorOptions} from "../injected";
 import type {Chain} from "@thirdweb-dev/chains";
-import {providers, utils} from "ethers";
+import {utils} from "ethers";
 import {getInjectedPelagusProvider} from "./getInjectedPelagusProvider";
-import {normalizeChainId} from "../../../lib/wagmi-core";
-import {getValidPublicRPCUrl} from "../../utils/url";
-
-export type PelagusConnectorOptions = Pick<InjectedConnectorOptions,
-  "shimDisconnect"> & {
-  /**
-   * While "disconnected" with `shimDisconnect`, allows user to select a different Pelagus account (than the currently connected account) when trying to connect.
-   */
-  UNSTABLE_shimOnConnectSelectAccount?: boolean;
-};
 
 type PelagusConnectorConstructorArg = {
   chains?: Chain[];
   connectorStorage: AsyncStorage;
-  options?: PelagusConnectorOptions;
+  options?: InjectedConnectorOptions;
 };
 
 export class PelagusConnector extends InjectedConnector {
+
   readonly id = walletIds.pelagus;
-  #UNSTABLE_shimOnConnectSelectAccount: PelagusConnectorOptions["UNSTABLE_shimOnConnectSelectAccount"];
 
   constructor(arg: PelagusConnectorConstructorArg) {
     const defaultOptions = {
@@ -52,8 +40,6 @@ export class PelagusConnector extends InjectedConnector {
       connectorStorage: arg.connectorStorage,
     });
 
-    this.#UNSTABLE_shimOnConnectSelectAccount =
-      options.UNSTABLE_shimOnConnectSelectAccount;
   }
 
   /**
@@ -75,7 +61,6 @@ export class PelagusConnector extends InjectedConnector {
       // `shimDisconnect` is active and account is in disconnected state (flag in storage)
       let account: string | null = null;
       if (
-        this.#UNSTABLE_shimOnConnectSelectAccount &&
         this.options?.shimDisconnect &&
         !Boolean(this.connectorStorage.getItem(this.shimDisconnectKey))
       ) {
@@ -86,10 +71,10 @@ export class PelagusConnector extends InjectedConnector {
           try {
             await provider.request({
               method: "wallet_requestPermissions",
-              params: [{quai_accounts: {}}],
+              params: [{eth_accounts: {}}],
             });
           } catch (error) {
-            // Not all MetaMask injected providers support `wallet_requestPermissions` (e.g. MetaMask iOS).
+            // Not all injected providers support `wallet_requestPermissions` (e.g. MetaMask iOS).
             // Only bubble up error if user rejects request
             if (this.isUserRejectedRequestError(error)) {
               throw new UserRejectedRequestError(error);
@@ -101,7 +86,7 @@ export class PelagusConnector extends InjectedConnector {
       // if account is not already set, request accounts and use the first account
       if (!account) {
         const accounts = await provider.request({
-          method: "quai_requestAccounts",
+          method: "eth_requestAccounts",
         });
         account = utils.getAddress(accounts[0] as string);
       }
@@ -148,147 +133,11 @@ export class PelagusConnector extends InjectedConnector {
     }
   }
 
-  /**
-   * @returns The first account address from the injected provider
-   */
-  async getAccount() {
-    const provider = await this.getProvider();
-    console.log("getAccount",provider)
-    if (!provider) {
-      throw new ConnectorNotFoundError();
-    }
-    const accounts = await provider.request({
-      method: "quai_accounts",
-    });
-
-    // return checksum address
-    // https://docs.ethers.org/v5/api/utils/address/#utils-getAddress
-    return utils.getAddress(accounts[0] as string);
-  }
-
-  /**
-   * @returns The `chainId` of the currently connected chain from injected provider normalized to a `number`
-   */
-  async getChainId() {
-    const provider = await this.getProvider();
-    console.log("getChainId",provider)
-    if (!provider) {
-      throw new ConnectorNotFoundError();
-    }
-    return provider.request({method: "quai_chainId"}).then(normalizeChainId);
-  }
-
-  /**
-   * get a `signer` for given `chainId`
-   */
-  async getSigner({chainId}: { chainId?: number } = {}) {
-    const [provider, account] = await Promise.all([
-      this.getProvider(),
-      this.getAccount(),
-    ]);
-
-    console.log("getSigner",provider, account)
-
-    // ethers.providers.Web3Provider
-    console.log(await new providers.Web3Provider(
-      provider as providers.ExternalProvider,
-      chainId,
-    ).getSigner(account));
-
-    return new providers.Web3Provider(
-      provider as providers.ExternalProvider,
-      chainId,
-    ).getSigner(account);
-  }
-
-  /**
-   * switch to given chain
-   */
-  async switchChain(chainId: number): Promise<Chain> {
-    console.log("switchChain")
-    const provider = await this.getProvider();
-    if (!provider) {
-      throw new ConnectorNotFoundError();
-    }
-
-    const chainIdHex = utils.hexValue(chainId);
-
-    try {
-      // request provider to switch to given chainIdHex
-      await provider.request({
-        method: "wallet_switchEthereumChain",
-        params: [{chainId: chainIdHex}],
-      });
-      const chain = this.chains.find((_chain) => _chain.chainId === chainId);
-      if (chain) {
-        return chain;
-      }
-
-      return {
-        chainId: chainId,
-        name: `Chain ${chainIdHex}`,
-        slug: `${chainIdHex}`,
-        nativeCurrency: {name: "Ether", decimals: 18, symbol: "ETH"},
-        rpc: [""],
-        chain: "",
-        shortName: "",
-        testnet: true,
-      };
-    } catch (error) {
-      // if could not switch to given chainIdHex
-
-      // if tried to connect to a chain that is not configured
-      const chain = this.chains.find((_chain) => _chain.chainId === chainId);
-      if (!chain) {
-        throw new ChainNotConfiguredError({chainId, connectorId: this.id});
-      }
-
-      // if chain is not added to provider
-      if (
-        (error as ProviderRpcError).code === 4902 ||
-        // Unwrapping for MetaMask Mobile
-        // https://github.com/MetaMask/metamask-mobile/issues/2944#issuecomment-976988719
-        (error as RpcError<{ originalError?: { code: number } }>)?.data
-          ?.originalError?.code === 4902
-      ) {
-        try {
-          // request provider to add chain
-          await provider.request({
-            method: "wallet_addEthereumChain",
-            params: [
-              {
-                chainId: chainIdHex,
-                chainName: chain.name,
-                nativeCurrency: chain.nativeCurrency,
-                rpcUrls: getValidPublicRPCUrl(chain), // no client id on purpose here
-                blockExplorerUrls: this.getBlockExplorerUrls(chain),
-              },
-            ],
-          });
-          return chain;
-        } catch (addError) {
-          // if user rejects request to add chain
-          if (this.isUserRejectedRequestError(addError)) {
-            throw new UserRejectedRequestError(error);
-          }
-
-          // else other error
-          throw new AddChainError();
-        }
-      }
-
-      if (this.isUserRejectedRequestError(error)) {
-        throw new UserRejectedRequestError(error);
-      }
-      throw new SwitchChainError(error);
-    }
-  }
-
   async switchAccount() {
     const provider = await this.getProvider();
     await provider.request({
       method: "wallet_requestPermissions",
-      params: [{quai_accounts: {}}],
+      params: [{eth_accounts: {}}],
     });
   }
 
